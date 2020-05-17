d8aab3ae90478861d3be9f85ccb9eddb552ad734
Natasha2805 <59359497+Natasha2805@users.noreply.github.com>
2020-05-17 15:30:21 +0300

    init: add part complete

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,13 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  switch (true) {
+    case date.getFullYear() % 4 !== 0: return false;
+    case date.getFullYear() % 100 !== 0: return true;
+    case date.getFullYear() % 400 !== 0: return false;
+    default: return true;
+  }
 }
 
 
@@ -74,7 +79,7 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+
 }
 
 
@@ -94,8 +99,13 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  let hr = date.getUTCHours();
+  const min = date.getUTCMinutes();
+  hr = Math.abs(Math.min(12 - hr, hr));
+  let degree = Math.abs(hr * 60 - min * 11);
+  if (degree > 360) degree -= 360;
+  return (0.5 * degree * Math.PI) / 180;
 }
 
 
